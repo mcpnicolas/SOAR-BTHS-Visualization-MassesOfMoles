71b7f136aed2e026285b0bc7dfacf7e18bd8bfb9
Maddie Nicolas <mpn272@nyu.edu>
2019-01-29 11:13:26 -0500

    added hover for unit info

---
+++ SOAR-BTHS-Visualization-MassesOfMoles/Assets/Scripts/HoverInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverInfo : MonoBehaviour {
+
+	public GameObject itemInfo1, itemInfo2;
+
+	public void enableInfo() {
+		itemInfo1.SetActive(true);
+		itemInfo2.SetActive(true);
+	}
+
+	public void disableInfo() {
+		itemInfo1.SetActive(false);
+		itemInfo2.SetActive(false);
+	}
+}
